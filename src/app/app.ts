9c0e0bc1c92121ebdaca7cb093625c862852ae0b
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-01-03 23:14:59 +0430

    the ngcomponent is done

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
+import { NgComponentOutlet } from "../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navbar, Footer],
+  imports: [RouterOutlet, Header, Navbar, Footer, NgComponentOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  constructor () {}
+   loadcomponent() {
+      return Footer ;
+    }
   protected readonly title = signal('my-angular-component');
+  newtitle:  string= "alawoddin";
+
 }
