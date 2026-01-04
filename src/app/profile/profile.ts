f79e014706c6d0c5d19d7f3d797d50fdcc77ed59
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-01-04 21:24:44 +0430

    the ng hook is done

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnChanges, SimpleChanges , OnInit, DoCheck } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges , OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ɵEmptyOutletComponent],
   templateUrl: './profile.html',
   styleUrls: ['./profile.css'],
 })
-export class Profile implements OnChanges, DoCheck , OnInit   {
+export class Profile implements OnChanges, DoCheck , OnInit , AfterContentInit, AfterContentChecked , AfterViewInit , OnDestroy ,  AfterViewChecked   {
 
   constructor() {
     console.log("this is for demo ")
@@ -25,6 +26,10 @@ export class Profile implements OnChanges, DoCheck , OnInit   {
     console.log("hello world");
   }
 
+  ngAfterContentInit(): void {
+    console.log("after content is user")
+  }
+
   ngDoCheck() {
     console.log("ngdocheck samething")
   }
@@ -32,6 +37,22 @@ export class Profile implements OnChanges, DoCheck , OnInit   {
 
   changebutton() {
     this.counter ++;
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("after content check the user")
+  }
+
+  ngAfterViewInit() {
+    console.log("check the user view")
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('check the the after view is working or not ')
+  }
+
+  ngOnDestroy(): void {
+    console.log("before you delete the item its call")
   }
  
 
