7d13afbf80cfd10e916882bd6ea514c47b1f71eb
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-01-04 21:39:47 +0430

    the pipe uppercase is done

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from "./header/header";
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
 import { Profile } from "./profile/profile";
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navbar, Footer, Profile],
+  imports: [RouterOutlet, Header, Navbar, Footer, Profile, UpperCasePipe , LowerCasePipe , TitleCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -20,6 +21,16 @@ export class App {
   }
 
   counter: number = 0;
+
+  oldtitle: string = 'ANGULAR APP';
+
+  uppercase() {
+    this.oldtitle = this.oldtitle.toUpperCase();
+  }
+
+  lowercase() {
+    this.oldtitle = this.oldtitle.toLowerCase();
+  }
   
   protected readonly title = signal('my-angular-component');
 
